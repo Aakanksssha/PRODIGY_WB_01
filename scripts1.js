7cae23ff2c9d78f4a60b511c76caf85ac8cb10d3
Aakanksha <160594489+Aakanksssha@users.noreply.github.com>
2024-09-26 21:01:56 +0530

    Add files via upload

---
+++ scripts1.js
@@ -0,0 +1,9 @@
+// Add class 'scrolled' to navbar when page is scrolled
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
